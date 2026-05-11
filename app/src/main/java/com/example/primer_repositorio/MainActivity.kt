0830752e20dfc57282006b6fbd04dd3764989fbd
DarthUlixes <jesus.uribe.23s@utzmg.edu.mx>
2026-05-11 11:28:19 -0600

    Primer avance proyecto Android

---
+++ app/src/main/java/com/example/primer_repositorio/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editTextNombre)
+        val button = findViewById<Button>(R.id.btnMostrar)
+        val textView = findViewById<TextView>(R.id.txtResultado)
+
+        button.setOnClickListener {
+
+            val texto = editText.text.toString()
+
+            // Pasar texto al label
+            textView.text = texto
+
+            // Mostrar alerta
+            AlertDialog.Builder(this)
+                .setTitle("Éxito")
+                .setMessage("El texto se mostró correctamente")
+                .setPositiveButton("OK", null)
+                .show()
+        }
+    }
+}
